cbde3bd31c45406c0fbce61161c50e60ff8fd8dc
wbruns <wbruns07@gmail.com>
2022-01-03 16:00:47 -0600

    started writing js, working with variables, functions and console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+// prompts and stores user input for player name
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// this logs a string and variable together?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+// fight();
